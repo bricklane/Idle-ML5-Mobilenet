b4dee4915f8f3052f5c091ba8c67bbabdff88029
Louise Macfadyen <louisejmacfadyen@gmail.com>
2020-07-21 10:23:55 -0700

    Add frame, new classes

--- classify.js
+++ classify.js
@@ -3,17 +3,22 @@ let vid;
 
 function preload() {
   classifier = ml5.imageClassifier('MobileNet')
-  vid = createVideo("clouds.mp4");
+  vid = createVideo("https://assets.codepen.io/192892/Clouds.mp4");
   vid.addClass('video_class');
-  vid.loop()
+  vid.loop();
+  vid.parent('frame')
+  //document.body.prepend(vid);
 }
+
 
 function setup() {
   createCanvas(0, 0);
   label = createDiv('label');  
   label.addClass('label_class');
+  label.parent('caption');
   confidence = createDiv('Confidence: ...');
   confidence.addClass('confidence_class');
+  confidence.parent('caption');
   //frameRate(1);
   classifyVideo();
   image(vid, 0, 0);
@@ -45,3 +50,7 @@ function gotResult(error, results) {
 // ref: https://creative-coding.decontextualize.com/video/
 // ref2: https://codepen.io/bricklane/pen/4a644d6ba54e9fd7d4c767c9489e9642
 //ref 3 https://editor.p5js.org/ml5/sketches/ImageClassification_DoodleNet_Video
+
+
+//Resources:
+// CSS Shadow:  https://neumorphism.io/#e8e8e8
