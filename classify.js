d5e18181717b38f302a03860b7b04d8bb40dbbeb
Louise Macfadyen <louisejmacfadyen@gmail.com>
2020-09-01 16:09:34 -0700

    Updated for full screen

--- classify.js
+++ classify.js
@@ -3,35 +3,29 @@ let vid;
 
 function preload() {
   classifier = ml5.imageClassifier('MobileNet')
-  vid = createVideo("clouds.mp4");
+  vid = createVideo("https://github.com/bricklane/Strato-ML5-Mobilenet/blob/master/Clouds3.mp4");
   vid.addClass('video_class');
-  vid.loop();
-  vid.parent('frame')
-  //document.body.prepend(vid);
+  vid.loop()
 }
 
-
 function setup() {
-  createCanvas(0, 0);
+  createCanvas(windowWidth, windowHeight);
   label = createDiv('label');  
   label.addClass('label_class');
-  label.parent('caption');
   confidence = createDiv('Confidence: ...');
   confidence.addClass('confidence_class');
-  confidence.parent('caption');
   //frameRate(1);
   classifyVideo();
   image(vid, 0, 0);
-  vid.play();
 }
 
 function classifyVideo() {
   classifier.classify(vid, gotResult);
 }
 
-//function draw() {
-//    console.log(frameCount); 
-//}
+function draw() {
+    //console.log(frameCount); 
+}
 
 function gotResult(error, results) {
   // Display error in the console
@@ -48,7 +42,6 @@ function gotResult(error, results) {
 }
 
 
-//ref: https://creative-coding.decontextualize.com/video/
-//ref 2: https://codepen.io/bricklane/pen/4a644d6ba54e9fd7d4c767c9489e9642
+// ref: https://creative-coding.decontextualize.com/video/
+// ref2: https://codepen.io/bricklane/pen/4a644d6ba54e9fd7d4c767c9489e9642
 //ref 3 https://editor.p5js.org/ml5/sketches/ImageClassification_DoodleNet_Video
-//ref 4 CSS Shadow:  https://neumorphism.io/#e8e8e8
